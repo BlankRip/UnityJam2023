71515739887ed434796ef0d9109180a21ce14585
BlankRip <aniketh_g5@outlook.com>
2023-06-02 23:01:55 +0530

    Updated Git Ignore & added folders to the project

---
+++ Assets/Scripts/DebugPerformanceProtection.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugPerformanceProtection : MonoBehaviour
+{
+     private void Awake() {
+#if UNITY_EDITOR
+    Debug.unityLogger.logEnabled = true;
+#else
+    Debug.unityLogger.logEnabled = false;
+#endif    
+    }
+}
