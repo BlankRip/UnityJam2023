9723cd992afb613499936a940f0f313a29f373fc
Sachit Nanajkar <sachitnjk@gmail.com>
2023-06-04 04:11:23 +0530

    TurretController_Added turret shoot functionality, turret shooting player when player enters turret trigger

--- Assets/Scripts/Turrets/TurretController.cs
+++ Assets/Scripts/Turrets/TurretController.cs
@@ -4,29 +4,34 @@ using UnityEngine;
 
 public class TurretController : MonoBehaviour
 {
+	[Header("References")]
 	[SerializeField] Transform turretTarget;
 	[SerializeField] Transform turretHead;
 	[SerializeField] Transform turretShootPoint;
 	[SerializeField] SphereCollider triggerCollider;
 
+	[Header("Turret main attributes")]
 	[SerializeField] private float rotationSpeed = 5f;
 	[SerializeField] private bool isInteractable;
+
+	[Header("Turret shoot attributes")]
+	[SerializeField] private GameObject turretProjectilePrefab;
+	[SerializeField] private float turretProjectileSpeed;
 
 	private bool targetDetected = false;
 
 	private void OnTriggerEnter(Collider other)
 	{
-		if(other.transform.gameObject == turretTarget.gameObject)
+		if(other.transform.gameObject.CompareTag("Player"))
 		{
 			targetDetected = true;
 		}
 	}
 	private void OnTriggerExit(Collider other)
 	{
-		if(other.transform.gameObject == turretTarget.gameObject)
+		if(other.transform.gameObject.CompareTag("Player"))
 		{
 			targetDetected = false;
-			//Standby();
 		}
 	}
 
@@ -35,6 +40,7 @@ public class TurretController : MonoBehaviour
 		if(targetDetected)
 		{
 			RotateTowardsTarget();
+			TurretShoot();
 		}
 		else
 		{
@@ -44,7 +50,6 @@ public class TurretController : MonoBehaviour
 
 	private void RotateTowardsTarget()
 	{
-		Debug.Log("will rotate towards target");
 		if(turretTarget != null)
 		{
 			Vector3 targetDirection = turretTarget.position - turretHead.position;
@@ -55,10 +60,27 @@ public class TurretController : MonoBehaviour
 	}
 	private void Standby()
 	{
-		Debug.Log("target has left the trigger area");
 		Quaternion initialRotation = Quaternion.identity;
 		turretHead.rotation = Quaternion.Slerp(turretHead.rotation, initialRotation, rotationSpeed * Time.deltaTime);
 	}
 
+	private void TurretShoot()
+	{
+		if(turretShootPoint != null && triggerCollider != null)
+		{
+			float maxRaycastDistance = triggerCollider.radius;
+			RaycastHit hit;
+			Vector3 rayDirection = turretTarget.position - turretShootPoint.position;
 
+			if(Physics.Raycast(turretShootPoint.position, rayDirection, out hit, maxRaycastDistance))
+			{
+				GameObject turretProjectile = Instantiate(turretProjectilePrefab, turretShootPoint.position, turretShootPoint.rotation);
+				Rigidbody projectileRB = turretProjectile.GetComponent<Rigidbody>();
+				if(projectileRB != null)
+				{
+					projectileRB.velocity = turretShootPoint.forward * turretProjectileSpeed;
+				}
+			}
+		}
+	}
 }
