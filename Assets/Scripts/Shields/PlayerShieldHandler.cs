22113603190f6a3f9405b5c280ddd266fd769b90
Sachit Nanajkar <sachitnjk@gmail.com>
2023-06-05 05:29:56 +0530

    PlayerShieldHandler_ player shield amount depletion when shields active functionality added, shield increases till max value then does not increase if max

--- Assets/Scripts/Shields/PlayerShieldHandler.cs
+++ Assets/Scripts/Shields/PlayerShieldHandler.cs
@@ -1,30 +1,50 @@
+using Gameplay;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class PlayerShieldHandler : MonoBehaviour
 {
 	[SerializeField] Slider shieldSlider;
 
+	[Header("Shield Attributes")]
+	[SerializeField] private int shieldDepletionAmount = 1;
+	[SerializeField] private int maxShield = 100;
+
 	private int currentShield;
-	private int maxShield = 100;
 
 	private int shieldDamageAmount = 10;
 
-	private bool shieldActive;
+	private bool shieldCanBeActivated;
+
+	InputAction ShieldTriggerInput;
 
 	private void Start()
 	{
-		shieldActive = true;
+		PlayerInput input = InputProvider.GetPlayerInput();
+		ShieldTriggerInput = input.actions["Shields"];
+
+		shieldCanBeActivated = true;
 		shieldSlider.maxValue = maxShield;
 		currentShield = maxShield;
 		shieldSlider.value = currentShield;
 	}
 
+	private void Update()
+	{
+		if(shieldCanBeActivated && ShieldTriggerInput.IsPressed())
+		{
+			currentShield -= shieldDepletionAmount;
+			currentShield = Mathf.Max(currentShield, 0);
+			shieldSlider.value = currentShield;
+		}
+	}
+
 	public void ShieldTakeDamage()
 	{
-		if(shieldActive && currentShield >= 1)
+		if(shieldCanBeActivated && currentShield >= 1)
 		{
 			currentShield -= shieldDamageAmount;
 			currentShield = Mathf.Max(currentShield, 0);
@@ -34,10 +54,10 @@ public class PlayerShieldHandler : MonoBehaviour
 
 	public void IncreaseShieldAmount(int regenAmount)
 	{
-		int newShieldValue = currentShield + regenAmount;
-		if(newShieldValue <= maxShield) 
-		{
-			currentShield = newShieldValue;
+		if(currentShield != maxShield) 
+		{ 
+			int newShieldValue = currentShield + regenAmount;
+			currentShield = Mathf.Min(newShieldValue, maxShield);
 			shieldSlider.value = currentShield;
 		}
 	}
@@ -49,6 +69,6 @@ public class PlayerShieldHandler : MonoBehaviour
 
 	public bool GetShieldStatus()
 	{
-		return shieldActive;
+		return shieldCanBeActivated;
 	}
 }
